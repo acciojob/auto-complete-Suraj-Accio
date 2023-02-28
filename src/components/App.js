2e7a89da370941deb9586857fb038f30524656d9
Suraj-Accio <124239927+Suraj-Accio@users.noreply.github.com>
2023-02-28 16:40:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
+import React, { useState, useEffect } from "react";
 
-import React from "react";
-import './../styles/App.css';
+const App = ({ suggestions }) => {
+  const [query, setQuery] = useState("");
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
 
-const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      setFilteredSuggestions(
+        suggestions.filter((suggestion) =>
+          suggestion.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    }, 500);
+  }, [query, suggestions]);
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" value={query} onChange={handleInputChange} />
+      <ul>
+        {filteredSuggestions.map((suggestion) => (
+          <li key={suggestion}>{suggestion}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
